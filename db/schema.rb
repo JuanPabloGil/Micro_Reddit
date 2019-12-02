d219163215f27f024bdb938d9963b4deea330e57
Juan Pablo <jpablomgil@gmail.com>
2019-12-02 15:08:58 -0600

    Creating the Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_203655) do
+ActiveRecord::Schema.define(version: 2019_12_02_205828) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
