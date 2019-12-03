8eaa3c5a24cb5877404800cecde11236c57d09f6
flov3rh <floperh@gmail.com>
2019-12-03 17:20:37 -0500

    fix redundance in models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  validates :user_id, presence: true
-  validates :post_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   belongs_to :user
   belongs_to :post
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 40 }
   validates :content, presence: true, length: { maximum: 140 }
-  validates :user_id, presence: true
   belongs_to :user
   has_many :comments
 end
